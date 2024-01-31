8634baf8cf12ff69a390dd128c31063cfebbb9a8
behnamrhp <behnamrahimpour74@gmail.com>
2024-01-31 23:58:50 +0300

    Added base logics to make it ready

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function test() {
-  console.log('hi');
-}
+import createUiLibraryTheme from '~/theme/themeCreator';
+import { paletteModes } from '~/configs/constants/default-configs';
+/* --------------------------------- exports -------------------------------- */
+export {
+  type ICustomUiLibraryTheme,
+  type IUiLibraryTheme,
+} from '~/configs/protocols';
+
+export { createUiLibraryTheme };
+export { paletteModes };
