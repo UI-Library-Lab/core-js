d06a52d5fd8d696ab9750ab9223b6c9ad5225d5d
behnamrhp <behnamrahimpour74@gmail.com>
2024-01-31 23:42:50 +0300

    Add basics of library

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "vite";
+import tsConfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
+import { glob } from "glob";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+
+export default defineConfig((configEnv) => ({
+  plugins: [
+    tsConfigPaths(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src/'],
+    }),
+    linterPlugin({
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      linters: [new EsLinter({ configEnv: configEnv }), new TypeScriptLinter()],
+    }),
+  ],
+  server: {
+    host: true,
+  },
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'Ui corejs',
+      formats: ['es'],
+      fileName: (format) => `ui-corejs.${format}.js`,
+    },
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob.sync("src/**/*.ts").map((file) => [
+          // The name of the entry point
+          // lib/nested/foo.ts becomes nested/foo
+          relative("src", file.slice(0, file.length - extname(file).length)),
+          // The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    }
+  }
+}));
