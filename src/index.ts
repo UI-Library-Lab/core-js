843643c24a3882369d8e62c43a2fc4ab69d1dbfc
behnamrhp <behnamrahimpour74@gmail.com>
2024-02-05 21:49:25 +0300

    Add some other exports for types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import createUiLibraryTheme from '~/theme/theme-creator';
 import { paletteModes } from '~/configs/constants/default-configs';
 /* --------------------------------- exports -------------------------------- */
-export {
-  type ICustomUiLibraryTheme,
-  type IUiLibraryTheme,
-} from '~/configs/protocols';
-
 export { createUiLibraryTheme };
 export { paletteModes };
+
+export {
+  type NTypographyConfigs,
+  type NPaletteConfigs,
+  type ShadowSizes,
+  type BlursConfigs,
+  type SpacingConfigs,
+  type BreakPoints,
+  type IUiLibraryTheme,
+  type ICustomUiLibraryTheme,
+} from '~/configs/protocols';
